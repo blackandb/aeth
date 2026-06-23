4258b8baed7539668c879e8359059d4c31f78ed9
blackandb <xeurxeur1@gmail.com>
2026-06-23 17:32:08 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { HeroSection } from "@/components/sections/hero";
-import { ShiftSection } from "@/components/sections/opportunity-shift";
+import { ShiftSection } from "@/components/sections/shift";
 import { NewModelSection } from "@/components/sections/new-model";
-import { HowWeBuildSection } from "@/components/sections/how-we-build";
-import { WhyBlackandSection } from "@/components/sections/why-blackand";
-import { WhoWeWorkWithSection } from "@/components/sections/who-we-work-with";
-import { OurPrinciplesSection } from "@/components/sections/our-principles";
-import { BuiltOnIntelligenceSection } from "@/components/sections/built-on-intelligence";
+import { HowItWorksSection } from "@/components/sections/how-it-works";
+import { BenefitsSection } from "@/components/sections/benefits";
+import { AudienceSection } from "@/components/sections/audience";
+import { SocialProofSection } from "@/components/sections/social-proof";
+import { FinalCtaSection } from "@/components/sections/final-cta";
 
 export default function Home() {
   return (
@@ -13,11 +13,11 @@ export default function Home() {
       <HeroSection />
       <ShiftSection />
       <NewModelSection />
-      <HowWeBuildSection />
-      <WhyBlackandSection />
-      <WhoWeWorkWithSection />
-      <OurPrinciplesSection />
-      <BuiltOnIntelligenceSection />
+      <HowItWorksSection />
+      <BenefitsSection />
+      <AudienceSection />
+      <SocialProofSection />
+      <FinalCtaSection />
     </main>
   );
 }
