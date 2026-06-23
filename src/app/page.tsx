c682972924c11405ccfe2327af0d8988282335c9
blackandb <xeurxeur1@gmail.com>
2026-06-23 16:53:49 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
 import { HeroSection } from "@/components/sections/hero";
-import { IntelligenceFoundationSection } from "@/components/sections/intelligence-foundation";
-import { EnduringAssetsSection } from "@/components/sections/enduring-assets";
-import { WhatWeBuildSection } from "@/components/sections/what-we-build";
-import { OperatingModelSection } from "@/components/sections/operating-model";
-import { CompaniesSection } from "@/components/sections/companies";
-import { WhyWeExistSection } from "@/components/sections/why-we-exist";
-import { PrinciplesSection } from "@/components/sections/principles";
-import { FinalCTASection } from "@/components/sections/final-cta";
+import { ShiftSection } from "@/components/sections/opportunity-shift";
+import { NewModelSection } from "@/components/sections/new-model";
+import { HowWeBuildSection } from "@/components/sections/how-we-build";
+import { WhyBlackandSection } from "@/components/sections/why-blackand";
+import { WhoWeWorkWithSection } from "@/components/sections/who-we-work-with";
+import { OurPrinciplesSection } from "@/components/sections/our-principles";
+import { BuiltOnIntelligenceSection } from "@/components/sections/built-on-intelligence";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <IntelligenceFoundationSection />
-      <EnduringAssetsSection />
-      <WhatWeBuildSection />
-      <OperatingModelSection />
-      <CompaniesSection />
-      <WhyWeExistSection />
-      <PrinciplesSection />
-      <FinalCTASection />
+      <ShiftSection />
+      <NewModelSection />
+      <HowWeBuildSection />
+      <WhyBlackandSection />
+      <WhoWeWorkWithSection />
+      <OurPrinciplesSection />
+      <BuiltOnIntelligenceSection />
     </main>
   );
 }
